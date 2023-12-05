87b725fcbe6284d502624e0621a7ebf9475fdd1a
Sufian Babri <9989266+SufianBabri@users.noreply.github.com>
2023-12-05 21:50:26 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Text>Hello, World!</Text>
+    </SafeAreaView>
+  );
+}
